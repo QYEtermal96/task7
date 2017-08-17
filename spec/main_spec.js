acf4ed783ff8050b11a0c71ee475b52c1c73d55e
e <1456958517@qq.com>
2017-08-17 10:24:05 +0800

    first

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -10,22 +10,21 @@ var main = require("../lib/main.js");
 
 
 describe("测试描述", function(){
-    sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
-    });
-
-    it("测试用例2", function(){
-
-        main();
+    it("read console.log when number = 2", function(){
+        sinon.spy(console, 'log');
+        main(3);
         var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
+        var expect_string ="3 bottles of beer on the wall, 3 bottles of beer.\n" +
+            "Take one down and pass it around, 2 bottles of beer on the wall.\n" +
+            "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+            "Take one down and pass it around, 1 bottle of beer on the wall.\n" +
+            "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+            "Take one down and pass it around, no more bottles of beer on the wall.\n" +
+            "No more bottles of beer on the wall, no more bottles of beer.\n" +
+            "Go to the store and buy some more, 99 bottles of beer on the wall.";
         expect(expect_string).to.equal(result);
     });
+
+
 });
